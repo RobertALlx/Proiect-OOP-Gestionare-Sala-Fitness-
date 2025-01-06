b6561cd1ca7b399a78df7bf0818ef09487fe7d75
Robert <balanrobert740@gmail.com>
2025-01-06 15:56:25 +0200

    Added Code to Clases

--- ConsoleApp1/ConsoleApp1/Clases/Abonat.cs
+++ ConsoleApp1/ConsoleApp1/Clases/Abonat.cs
@@ -1,6 +1,86 @@
-namespace ConsoleApp1.Clases;
+using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
-public class Abonat
+namespace ConsoleApp1
 {
-    
+    public class Abonat
+    {
+        private string _nume;
+        private string _cnp;
+        private string _username;
+        private string _password;
+        private string _tipAbonament;
+        private double _taxaSuplimentara; 
+        private List<Antrenament> _istoricAntrenamente = new List<Antrenament>();
+        
+        public string Nume { get => _nume; set => _nume = value; }
+        public string Cnp { get => _cnp; set => _cnp = value; }
+        public string Username { get => _username; set => _username = value; }
+        public string Password { get => _password; set => _password = value; }
+        public string TipAbonament { get => _tipAbonament; set => _tipAbonament = value; }
+        public double TaxaSuplimentara { get => _taxaSuplimentara; }
+        public IReadOnlyList<Antrenament> IstoricAntrenamente => _istoricAntrenamente.AsReadOnly();
+
+        public Abonat(string nume, string cnp, string username, string password, string tipAbonament)
+        {
+            _nume = nume;
+            _cnp = cnp;
+            _username = username;
+            _password = password;
+            _tipAbonament = tipAbonament;
+            _taxaSuplimentara = 0; // Initializare taxa suplimentara
+        }
+
+        public void AdaugaAntrenament(DateTime dataOra, TimeSpan durata, string antrenor)
+        {
+            _istoricAntrenamente.Add(new Antrenament(dataOra, durata, antrenor));
+        }
+
+        public void AfiseazaIstoricAntrenamente()
+        {
+            Console.WriteLine($"Istoricul antrenamentelor pentru {Nume}:");
+            foreach (var antrenament in _istoricAntrenamente)
+            {
+                Console.WriteLine(antrenament);
+            }
+        }
+
+        // Metoda pentru adaugarea taxei suplimentare
+        public void AdaugaTaxaSuplimentara(double taxa)
+        {
+            _taxaSuplimentara += taxa; // Adauga taxa suplimentara la valoarea existenta
+            Console.WriteLine($"Taxa suplimentara de {taxa} RON a fost adaugata la contul abonatului {_nume}.");
+            Console.WriteLine($"Total taxe suplimentare pentru {_nume}: {_taxaSuplimentara} RON.");
+        }
+        
+        public double CalculeazaCostAbonament()
+        {
+            double costLunar = 0;
+
+            if (_tipAbonament == "Standard")
+            {
+                costLunar = 100; // Costul abonamentului Standard
+            }
+            else if (_tipAbonament == "Premium")
+            {
+                costLunar = 150; // Costul abonamentului Premium
+            }
+
+            // Adauga taxa suplimentara la costul lunar
+            return costLunar + _taxaSuplimentara;
+        }
+
+        // Validare CNP
+        private bool ValidareCnp(string cnp)
+        {
+            return Regex.IsMatch(cnp, @"^[1-8]\d{12}$");
+        }
+
+        // Suprascrierea ToString pentru detalii abonat
+        public override string ToString()
+        {
+            return $"Abonat: {_nume}, CNP: {_cnp}, Username: {_username}, Tip Abonament: {_tipAbonament}, Taxe Suplimentare: {_taxaSuplimentara} RON";
+        }
+    }
 }
--- ConsoleApp1/ConsoleApp1/Clases/Programare.cs
+++ ConsoleApp1/ConsoleApp1/Clases/Programare.cs
@@ -1,6 +1,98 @@
-namespace ConsoleApp1.Clases;
+using System;
 
-public class Programare
+namespace ConsoleApp1
 {
-    
+    public class Programare
+    {
+        private Abonat _abonat;
+        private Antrenor _antrenor;
+        private DateTime _dataOra;
+        private double _durata; // Durata rezervata in ore
+        private double _durataRealizata; // Durata reala in ore
+
+        public Abonat Abonat { get => _abonat; set => _abonat = value; }
+        public Antrenor Antrenor { get => _antrenor; set => _antrenor = value; }
+        public DateTime DataOra { get => _dataOra; set => _dataOra = value; }
+        public double Durata { get => _durata; set => _durata = value; }
+        public double DurataRealizata { get => _durataRealizata; set => _durataRealizata = value; }
+        public double TaxaSuplimentara { get; private set; } = 0;
+        public double TaxaPenalizare { get; private set; } = 0;  // Taxa penalizare pentru anulare
+
+        public Programare(Abonat abonat, Antrenor antrenor, DateTime dataOra, double durata)
+        {
+            _abonat = abonat;
+            _antrenor = antrenor;
+            _dataOra = dataOra;
+            _durata = durata;
+            _durataRealizata = durata; // Initial, se presupune ca durata realizata este cea rezervata
+        }
+
+        // Metoda pentru calcularea taxelor suplimentare
+        public void CalculeazaTaxaSuplimentara()
+        {
+            double oreSuplimentare = _durataRealizata - _durata;
+
+            if (oreSuplimentare > 0)
+            {
+                if (_abonat.TipAbonament == "Standard")
+                {
+                    TaxaSuplimentara = oreSuplimentare * 5; // 5 RON/ora pentru abonati standard
+                }
+                else if (_abonat.TipAbonament == "Premium")
+                {
+                    TaxaSuplimentara = oreSuplimentare * 2; // 2 RON/ora pentru abonati premium
+                }
+
+                // Adaugam taxa in contul abonatului
+                _abonat.AdaugaTaxaSuplimentara(TaxaSuplimentara);
+
+                Console.WriteLine($"Taxa suplimentara de {TaxaSuplimentara} RON a fost adaugata pentru abonatul {_abonat.Nume}.");
+            }
+        }
+
+        // Metoda pentru anularea programarii
+        public void AnulareProgramare()
+        {
+            var oraCurenta = DateTime.Now;
+            var diferentaOre = (DataOra - oraCurenta).TotalHours;
+
+            if (diferentaOre < 24)
+            {
+                TaxaPenalizare = 10;  // Penalizare de 10 RON pentru anulare cu mai putin de 24 de ore
+                _abonat.AdaugaTaxaSuplimentara(TaxaPenalizare); // Adaugam taxa penalizare la contul abonatului
+
+                Console.WriteLine($"Programarea pentru {_abonat.Nume} cu antrenorul {_antrenor.NumeComplet} a fost anulata. Penalizare aplicata de {TaxaPenalizare} RON.");
+            }
+            else
+            {
+                Console.WriteLine($"Programarea pentru {_abonat.Nume} cu antrenorul {_antrenor.NumeComplet} a fost anulata fara penalizare.");
+            }
+        }
+
+        // Metoda pentru modificarea programarii
+        public void ModificareProgramare(DateTime nouaDataOra, double nouaDurata)
+        {
+            // Modificam doar daca diferenta de timp este suficient de mare
+            var oraCurenta = DateTime.Now;
+            var diferentaOre = (DataOra - oraCurenta).TotalHours;
+
+            if (diferentaOre >= 24)
+            {
+                _dataOra = nouaDataOra;  // Modificam data si ora programarii
+                _durata = nouaDurata;     // Modificam durata
+
+                Console.WriteLine($"Programarea pentru {_abonat.Nume} cu antrenorul {_antrenor.NumeComplet} a fost modificata la {nouaDataOra} cu durata de {nouaDurata} ore.");
+            }
+            else
+            {
+                Console.WriteLine($"Nu se poate modifica programarea pentru {_abonat.Nume} cu antrenorul {_antrenor.NumeComplet}, deoarece este mai putin de 24 de ore pana la programare.");
+            }
+        }
+
+        // Suprascrierea metodei ToString pentru afisarea detaliilor programarii
+        public override string ToString()
+        {
+            return $"Programare: {_abonat.Nume}, Antrenor: {_antrenor.NumeComplet}, Data: {_dataOra}, Durata rezervata: {_durata} ore, Durata realizata: {_durataRealizata} ore, Taxa suplimentara: {TaxaSuplimentara} RON, Taxa penalizare: {TaxaPenalizare} RON";
+        }
+    }
 }
