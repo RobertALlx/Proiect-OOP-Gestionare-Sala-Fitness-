3a58c48d7753c8be002ef11edf21c9ec96364908
Coso17 <162422500+Coso17@users.noreply.github.com>
2025-01-06 15:53:14 +0200

    Update AbonatPremium.cs

--- ConsoleApp1/ConsoleApp1/Clases/AbonatPremium.cs
+++ ConsoleApp1/ConsoleApp1/Clases/AbonatPremium.cs
@@ -2,8 +2,6 @@ namespace ConsoleApp1.Clases;
 
 public class AbonatPremium:Abonat
 {
-    private const int _extraOre = 2; // Numar fix de ore suplimentare
-
-    public int ExtraOre => _extraOre;
+    
 
 }
