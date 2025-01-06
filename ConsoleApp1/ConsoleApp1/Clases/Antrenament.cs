e7843971bb6ac78b3ab4cb9323dcb8113f9b4d77
Coso17 <162422500+Coso17@users.noreply.github.com>
2025-01-06 16:01:24 +0200

    Update Antrenament.cs

--- ConsoleApp1/ConsoleApp1/Clases/Antrenament.cs
+++ ConsoleApp1/ConsoleApp1/Clases/Antrenament.cs
@@ -2,5 +2,18 @@ namespace ConsoleApp1.Clases;
 
 public class Antrenament
 {
-    
+    private DateTime _dataOra;
+    private TimeSpan _durata;
+    private string _antrenor;
+
+    public DateTime DataOra{get=>_dataOra;set=>_dataOra=value;}
+    public TimeSpan Durata{get=>_durata;set=>_durata=value;}
+    public string Antrenor{get=>_antrenor; set=>_antrenor=value;}
+
+    public Antrenament(DateTime dataOra, TimeSpan durata, string antrenor)
+    {
+        _dataOra = dataOra;
+        _durata = durata;
+        _antrenor = antrenor;
+    }
 }
